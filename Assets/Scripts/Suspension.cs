4cb90b8edd68d069648561924e384c99e3af7b28
LAPTOP-DH3ESM33\StaplesDE <alina.hw@web.de>
2018-04-19 14:08:18 +0200

    Hover Car + Car Suspension

---
+++ Assets/Scripts/Suspension.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Suspension : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    private Car carScript;
+
+    [Header("Suspension")]
+    public float springForce;
+    public float damperForce;
+    public float springConstant;
+    public float damperConstant;
+    public float restLenght;
+
+    private float previousLenght;
+    private float currentLenght;
+    private float springVelocitiy;
+
+    private void Start()
+    {
+        carScript = transform.parent.GetComponent<Car>();
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, -transform.up, out hit, restLenght + carScript.wheelRadius))
+        {
+            previousLenght = currentLenght;
+            currentLenght = restLenght - (hit.distance - carScript.wheelRadius);
+            springVelocitiy = (currentLenght - previousLenght) / Time.fixedDeltaTime;
+            springForce = springConstant * currentLenght;
+            damperForce = damperConstant * springVelocitiy;
+
+            rigidbody.AddForceAtPosition(transform.up * (springForce + damperForce), transform.position);
+        }
+    }
+}
