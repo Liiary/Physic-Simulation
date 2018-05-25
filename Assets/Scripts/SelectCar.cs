93635aa5f683d0a4437e9f493cef11906c373e52
Lia <alina.hw@web.de>
2018-05-25 16:27:51 +0200

    CarSelection Secen + Script

---
+++ Assets/Scripts/SelectCar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectCar : MonoBehaviour
+{
+    public GameObject[] Cars;
+    public int CarNumber;
+
+    public void SelectingNextCar()
+    {
+        Cars[CarNumber].gameObject.SetActive(false);
+        CarNumber++;
+
+        if (CarNumber == 3)
+        {
+            CarNumber = 0;
+        }
+
+        Cars[CarNumber].gameObject.SetActive(true);
+    }
+
+    public void SelectingPreviousCar()
+    {
+        Cars[CarNumber].gameObject.SetActive(false);
+        CarNumber--;
+
+        if(CarNumber == -1)
+        {
+            CarNumber = 2;
+        }
+
+        Cars[CarNumber].gameObject.SetActive(true);
+    }
+}
